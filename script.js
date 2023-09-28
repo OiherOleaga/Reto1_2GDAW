05976124628f6ab4921bd5eda38ec28b6881caba
Anartz <anartz.pagaldai@ikasle.egibide.org>
2023-09-28 09:17:23 +0200

    animacion de elegir parada

---
+++ script.js
@@ -0,0 +1,39 @@
+let imgTranvia = document.querySelector("#tranvia")
+let divParadas = document.querySelector(".paradas")
+let botonMarcha = document.querySelector("#marcha")
+let paradaActual = 1 // el que se reciva del servidor
+let paradaDestino = 1
+//let direcionDerecha = true;
+for (let parada of divParadas.children) {
+    parada.addEventListener("click", () => {
+        switch(parada.id) {
+            case "parada1":
+                paradaDestino = 1
+                break
+            case "parada2":
+                paradaDestino = 2
+                break
+            case "parada3":
+                paradaDestino = 3
+                break
+            case "parada4":
+                paradaDestino = 4
+                break
+            case "parada5":
+                paradaDestino = 5
+                break
+        }
+        let segundos = Math.abs(paradaDestino - paradaActual)
+        let posicion = parada.getBoundingClientRect().left - (imgTranvia.offsetWidth)
+        imgTranvia.style.transition =  `transform ${segundos}s linear` // ease
+        imgTranvia.style.transform = `translate(${posicion}px, 0)`
+        paradaActual = paradaDestino;
+    })
+}
+// primero hace falta la comunicacion con el servidor
+/* 
+botonMarcha.addEventListener("click", () => {
+    divParadas.children[paradaActual == 5 ?]
+    imgTranvia.style.transform = `translate(${posicion}px, 0)`
+})
+*/
