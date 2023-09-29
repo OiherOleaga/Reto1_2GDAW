f29de9a0793c2be4d8329aed709b7a0c69939416
David Moreno Alonso <david.moreno@ikasle.egibide.org>
2023-09-29 09:18:30 +0200

    animacion auto

--- script.js
+++ script.js
@@ -25,20 +25,30 @@ for (let parada of divParadas.children) {
         }
         let segundos = Math.abs(paradaDestino - paradaActual)
         let posicion = parada.getBoundingClientRect().left - (imgTranvia.offsetWidth)
-        imgTranvia.style.transition = `transform ${segundos}s linear` // ease
+        imgTranvia.style.transition = `transform ${segundos}s ease` // ease
         imgTranvia.style.transform = `translate(${posicion}px, 0)`
         paradaActual = paradaDestino;
     })
 }
 function mostrarLista() {
-    lista=document.getElementById("estadisticas");
-    if(lista.style.display==="none"||lista.style.display === ""){
+    lista = document.getElementById("estadisticas");
+    if (lista.style.display === "none" || lista.style.display === "") {
         lista.style.display = "block";
-    }else lista.style.display = "none";
+    } else lista.style.display = "none";
+}
+
+function moverTranviaAuto() {
+    tranvia = document.getElementById("tranvia")
+    tranvia.style.animation = "mover 5s ease"
+}
+
+function botonMarchaAnimacion() {
+    boton=document.getElementById("marcha")
+    boton.style.animation= "reducir .3s ease"
 }
 
 document.getElementById("menu").addEventListener("click", mostrarLista)
-
+document.getElementById("marcha").addEventListener("click", botonMarchaAnimacion)
 
 
 
@@ -49,30 +59,31 @@ botonMarcha.addEventListener("click", () => {
     imgTranvia.style.transform = `translate(${posicion}px, 0)`
 })
 */
+
 let href = window.location.href
-botonMarcha.addEventListener("click",async () => {
+botonMarcha.addEventListener("click", async () => {
     await postVariable("RESET", 1, true)
     postVariable("RESET", 0, false)
     await postVariable("MARTXA", 1, true)
     postVariable("MARTXA", 0, false)
-    let interval = setInterval(async() => {
+    let interval = setInterval(async () => {
         let ET0 = await (await fetch('ET0.html')).text()
         if (ET0 === 1) {
             await postVariable("INICIO", 1, true)
             postVariable("INICIO", 0, false)
-            clearInterval(interval)  
+            clearInterval(interval)
         }
     }, 100)
 })
 
 async function postVariable(variable, valor, espera) {
     if (espera) {
-        await fetch(href, { 
+        await fetch(href, {
             method: "post",
             body: `%22Tabla+de+variables+est%C3%A1ndar%22.${variable}=${valor}`
         })
     } else {
-        fetch(href, { 
+        fetch(href, {
             method: "post",
             body: `%22Tabla+de+variables+est%C3%A1ndar%22.${variable}=${valor}`
         })
